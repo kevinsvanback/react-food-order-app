f7afd1536bf493459e364d7ca0d0070a67604f26
Kevin <kevin.svanback@gmail.com>
2022-01-26 13:25:46 +0100

    Cart button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,11 +1,17 @@
-
+import foodImage from '../../assets/food.jpg';
+import styles from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
   return (
     <>
-      <header>
+      <header className={styles.header}>
         <h1>React Food Order App</h1>
+        <HeaderCartButton />
       </header>
+      <div className={styles['main-image']}>
+        <img src={foodImage} alt="header-img" />
+      </div>
     </>
   );
 };
---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,14 @@
+import styles from './HeaderCartButton.module.css';
+import CartIcon from '../Cart/CartIcon';
+
+const HeaderCartButton = (props) => {
+  return (
+    <button className={styles.button}>
+      <span className={styles.icon}><CartIcon /></span>
+      <span>Cart</span>
+      <span className={styles.badge}>0</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
