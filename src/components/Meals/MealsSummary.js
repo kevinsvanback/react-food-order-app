99d9e5faa88530c312342ce840cbc37dc3145559
Kevin <kevin.svanback@gmail.com>
2022-01-26 13:52:36 +0100

    Meals and dummy data

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,13 @@
+import styles from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={styles.summary}>
+      <h2>Order Food To Your Door</h2>
+      <p>Paragraph one</p>
+      <p>Paragraph two</p>
+    </section>
+  );
+};
+
+export default MealsSummary;
