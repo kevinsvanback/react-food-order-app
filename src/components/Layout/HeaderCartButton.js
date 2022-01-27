b90ef71f11b75513177a8f08223bc7d0f5de7b97
Kevin <kevin.svanback@gmail.com>
2022-01-27 14:37:11 +0100

    Bump animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,37 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import styles from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((currentNum, item) => { return currentNum + item.amount; }, 0);
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((currentNum, item) => { return currentNum + item.amount; }, 0);
+
+  const btnClasses = `${styles.button} ${btnIsHighlighted ? styles.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={styles.icon}><CartIcon /></span>
       <span>Cart</span>
       <span className={styles.badge}>{numberOfCartItems}</span>
